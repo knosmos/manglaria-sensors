1366874a824dcb23afd6a79d690f2c93c3f5479f
Jieruei Chang <knosmos@gmail.com>
2025-08-07 14:51:02 -0400

    quick fix for mobile view

--- src/app/splash.tsx
+++ src/app/splash.tsx
@@ -18,7 +18,7 @@ export default function SplashScreen() {
     if (!visible) return null; // Don't render if not visible
 
     return (
-        <div className={`fixed inset-0 flex flex-col items-center justify-center z-50 bg-gray-300 transition-opacity duration-500 ease-in-out ${fadeoutStarted ? 'opacity-0' : 'opacity-100'}`}>
+        <div className={`fixed inset-0 flex flex-col items-center justify-center z-50 bg-gray-300 text-black transition-opacity duration-500 ease-in-out ${fadeoutStarted ? 'opacity-0' : 'opacity-100'}`}>
         <h1 className="text-8xl font-sans tracking-widest">MANGLARIA</h1>
         <p className="text-xl font-mono mt-4 tracking-widest">LOADING MAP ///////////</p>
         </div>
